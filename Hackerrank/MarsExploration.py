e81b7ea6f02be9c533d738fb5c67651704398b09
Cesar Arcos <60860385+CAG9@users.noreply.github.com>
2022-04-06 14:15:05 -0500

    Create MarsExploration.py

---
+++ Hackerrank/MarsExploration.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marsExploration' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def marsExploration(s):
+    
+    stringlen = len(s)/3
+    DesireMessage  = 'SOS' * int(stringlen)
+    result = 0
+    for i in range(len(s)):
+        if s[i] != DesireMessage[i]:
+            result += 1
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = marsExploration(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
