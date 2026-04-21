0db95893b5d3303a0bf1b67f43e78571787dff65
Cesar Arcos Gonzalez <60860385+CAG9@users.noreply.github.com>
2026-04-20 20:43:17 -0600

    Update 12_Integer_to_Roman.py

--- LeetCode/12_Integer_to_Roman.py
+++ LeetCode/12_Integer_to_Roman.py
@@ -5,3 +5,6 @@ class Solution:
         X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
         I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
         return M[num // 1000] + C[(num % 1000) // 100] + X[(num % 100) // 10] + I[num % 10]
+
+
+
